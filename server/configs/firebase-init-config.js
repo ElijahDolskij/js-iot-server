ce241b8b67fe96d60d973d19e6db725916506747
Илья <ElijahDolskij@gmail.com>
2018-06-12 22:09:45 +0300

    Fix create directory method

---
+++ server/configs/firebase-init-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "type": "service_account",
+  "project_id": "iot-server-test-storage",
+  "private_key_id": "08506168ce1e657f4ee7b79f0b83da800d3cd78c",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC8VLkidOkIZRwo\n+XzbEqmEFwQQHsbjAhJsrCQqAbXcArwI22YX7w6yJgCA5PzwKJGmlrtj3OWgBP11\njbe+jbzybwgs8zuUfODIxZLevD118poXwJqy5SzPsn7j+VjIZcJLUzHt/nEedJgG\nvIgQypo4k9iBq2vkGhxLCOvuotBjniqTQGuoPqSfHGzEduhTuxdhFHHUTcCJ5gra\nfM/bGjsc3J0fXT7Iz0kgWXF45MPgWeYckemxbbo/mBsZiuCSZHbOBz9MZXMfFGHk\n8Xjso7Kc9RmYqOuG5sw6Albam7W5hEoydcMSI2tRHMT2D4jr9rOfJxSmggeFk8pv\np97AOAAtAgMBAAECggEACdkYUT/xy5mV7mKVb9M7AAhAEyO43LgIBJy/y16Jhvax\npHjsjsXWa+Rm1FalD13ft+lb5S3w5/w60MHXYOSCg97ge5FAhVQ+YOFL+8+2GwvT\nmPheXopzXG0GC5/HT7tafJveGYbbnW8VYAzNxKsDg3/fq3AuVSA0aIwP4kurP/o3\nXnUHruQUKgYzAKTN0yxC004E4NUpN6nZvunvTURpswIalUUQ15NlJoMw6aTdOpdp\nRTNXe5AEL/+mx6zEbPQgUOtnW0lcCRBm6MjFhBQLt71f1MJBe2x9jdz7OAR6n8vr\nla9jjMTA7kZqGQQK0HiOhZhsr+C66CuIk++3HQxnVQKBgQDkWp+VorvMrClbeJNc\nQAY5iIP/dp2NlAsLxXxL+R0c2ZyPtoIj5KSyvPlh0DQgWYj500+LHMj8GUhuK4Hx\nkObYOJ+QUOlDlWgdbGRjeSwHWqW1B8SxRtZLv/fOyB+5FZJLAKOlwiAuq1MkYcxH\njWNU6qBOrunP+I6hoZTBdfhMYwKBgQDTIapQA5Yv3UJSvwR0jERxHSa7Nd6oxt0O\ntk4dASkLezdEqQzY9QfyWhY5lBbWz+aFmfcYODODzp/YmwBV48sQbKOl0TnApmZs\nkkWlta9pPUxctmKHXZLIKcW50rZa5HI61d2s81xgpruhXmSVqx3Kj9oxcG3zzfof\nW4lJmvY+LwKBgQDTI0ey34itMfH7dX0rKreWsrb5mf8LTPr5o1vOE0PrzPmJl8K0\n1U/DRB+BZTUNRDv2Yn+FrNPY5LenxeqcJNKM0a4XjfhgWOCvc+1OXFdtZU8aF1iy\nYUD7QUArnbZXN67QUoL9XITNIfP6C8MJ1Z0mgto+ZlQhyzx2DiSwzBcM6QKBgQCO\nNZvQ/0l2/fKBZ7Zphl1HT1paOQO77Q7SrQraUIDEAf+NGMzVid4LaJCVweDMWgas\nsLMVti1Et7VFIcpND/0kF/j2ZIIg+DP/J1sQLdtz6Fz6CcM96FJIzpInAv3fJQao\nCIuixdzQIR46qCjaAwMzAEf4iwEUegXifzZuxJjd8wKBgAE9Syk+uR2M96yw35aA\nKinA1MuNpwHjVpb9f0ueWzw2pbj145UOXfvhTbJommISiezsG2RKXzvOltDUVATu\nOs6R9KPa9km4thAgUy9dpy4PeOcgQcB4INhXSiK6aW0RE+BwbLFpvkaJCiR0HGan\nwuHrFDbUX79wAvkWJXaZqYAT\n-----END PRIVATE KEY-----\n",
+  "client_email": "firebase-adminsdk-68qwx@iot-server-test-storage.iam.gserviceaccount.com",
+  "client_id": "112492979745833508586",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://accounts.google.com/o/oauth2/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-68qwx%40iot-server-test-storage.iam.gserviceaccount.com"
+}
+;
